3761035642651ae430475cddb9359d76378a583c
Dev Raj Singh <dev.maildrop@gmail.com>
2025-08-05 22:50:07 +0530

    removed [ https:// ] Render domain to ALLOWED_HOSTS

--- text_summarizer/settings.py
+++ text_summarizer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://text-summarizer-ypia.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["text-summarizer-ypia.onrender.com", "localhost", "127.0.0.1"]
 
 # Application definition
 
